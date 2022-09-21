11f623e14942c5b070689fe5d1404dbf8ba50eac
NerosJourney <sandman4264@gmail.com>
2022-09-21 00:18:09 -0400

    Moved account methods into account manager

---
+++ src/account_manager.py
@@ -0,0 +1,33 @@
+from db_builder import db, cur
+
+def get_current_holding(account, ticker):
+    try:
+        cur.execute(f'SELECT quantity, avg_price FROM Holdings WHERE ticker="{ticker}" AND account_id="{account}"')
+        holdings = cur.fetchone()
+        curr_quantity = holdings[0]
+        curr_avg_price = holdings[1]
+    except:
+        cur.execute(f'INSERT INTO Holdings (account_id, ticker, quantity, avg_price) VALUES ({account}, "{ticker}", 0, 0)')
+        curr_quantity = 0
+        curr_avg_price = 0
+        db.commit()
+    return curr_quantity, curr_avg_price
+
+
+def update_current_holding(account, ticker, quantity, avg):
+    cur.execute(f'UPDATE Holdings SET quantity = {quantity}, avg_price = {avg} WHERE ticker="{ticker}" AND account_id="{account}"')
+    db.commit()
+
+
+def get_account_cash(account):
+    cur.execute(f'SELECT cash FROM Accounts WHERE account_id="{account}"')
+    bal = cur.fetchone()
+
+    if not bal:
+        print("This account does not exist")
+        return None
+    return bal[0]
+
+def update_account_cash(account, cash):
+    cur.execute(f'UPDATE Accounts SET cash = {cash} WHERE account_id="{account}"')
+    db.commit()
--- src/stock_manager.py
+++ src/stock_manager.py
@@ -1,5 +1,6 @@
 from asyncio.windows_events import NULL
 from db_builder import db, cur
+from account_manager import *
 
 def buy_stock(account, ticker, quantity):
     cur.execute(f'SELECT curr_price FROM Stocks WHERE ticker="{ticker}"')
@@ -37,38 +38,3 @@ def sell_stock(account, ticker, quantity):
     curr_bal = get_account_cash(account)
     update_account_cash(account, curr_bal + (quantity * price))
     return True
-
-
-def get_current_holding(account, ticker):
-    try:
-        cur.execute(f'SELECT quantity, avg_price FROM Holdings WHERE ticker="{ticker}" AND account_id="{account}"')
-        holdings = cur.fetchone()
-        curr_quantity = holdings[0]
-        curr_avg_price = holdings[1]
-    except:
-        cur.execute(f'INSERT INTO Holdings (account_id, ticker, quantity, avg_price) VALUES ({account}, "{ticker}", 0, 0)')
-        curr_quantity = 0
-        curr_avg_price = 0
-        db.commit()
-    return curr_quantity, curr_avg_price
-
-
-def update_current_holding(account, ticker, quantity, avg):
-    cur.execute(f'UPDATE Holdings SET quantity = {quantity}, avg_price = {avg} WHERE ticker="{ticker}" AND account_id="{account}"')
-    db.commit()
-
-
-def get_account_cash(account):
-    cur.execute(f'SELECT cash FROM Accounts WHERE account_id="{account}"')
-    bal = cur.fetchone()
-
-    if not bal:
-        print("This account does not exist")
-        return None
-    return bal[0]
-
-def update_account_cash(account, cash):
-    cur.execute(f'UPDATE Accounts SET cash = {cash} WHERE account_id="{account}"')
-    db.commit()
-
-sell_stock(1, "AAPL", 50)
