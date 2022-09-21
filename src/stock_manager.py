6c057495f077fc6c32bfcc9002263bf39eb74f48
NerosJourney <sandman4264@gmail.com>
2022-09-20 23:52:38 -0400

    buy_stock now updates the DB properly

--- src/stock_manager.py
+++ src/stock_manager.py
@@ -9,8 +9,12 @@ def buy_stock(account, ticker, quantity):
         curr_quantity = holdings[0]
         curr_avg_price = holdings[1]
     except:
+        cur.execute(f'INSERT INTO Holdings (account_id, ticker, quantity, avg_price) VALUES ({account}, "{ticker}", 0, 0)')
         curr_quantity = 0
         curr_avg_price = 0
-    print(price, curr_quantity, curr_avg_price)
+    new_quantity = curr_quantity + quantity
+    total_holdings_price = (curr_quantity * curr_avg_price) + (quantity * price)
+    new_avg = total_holdings_price / new_quantity
+    cur.execute(f'UPDATE Holdings SET quantity = {new_quantity}, avg_price = {new_avg} WHERE ticker="{ticker}" AND account_id="{account}"')
+    db.commit()
 
-buy_stock(1, 'AAPL', 1)
