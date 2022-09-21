f93328b427d3eac42d34c94d62d1d23238126d3d
NerosJourney <sandman4264@gmail.com>
2022-09-21 00:17:07 -0400

    Added account balance checking to buy and sell stock

--- src/stock_manager.py
+++ src/stock_manager.py
@@ -1,16 +1,24 @@
+from asyncio.windows_events import NULL
 from db_builder import db, cur
 
 def buy_stock(account, ticker, quantity):
     cur.execute(f'SELECT curr_price FROM Stocks WHERE ticker="{ticker}"')
     price = cur.fetchone()[0]
 
+    acc_bal = get_account_cash(account)
+
+    if(quantity * price > acc_bal):
+        print("Not enough money :(")
+        return False
+
     curr_quantity, curr_avg_price = get_current_holding(account, ticker)
     new_quantity = curr_quantity + quantity
-    
+
     total_holdings_price = (curr_quantity * curr_avg_price) + (quantity * price)
     new_avg = total_holdings_price / new_quantity
 
     update_current_holding(account, ticker, new_quantity, new_avg)
+    update_account_cash(account, acc_bal - (quantity * price))
     return True
 
 
@@ -19,8 +27,15 @@ def sell_stock(account, ticker, quantity):
     if(quantity > curr_quantity):
         print("ERROR! YOU DON'T OWN THAT STOCK")
         return False
+
+    cur.execute(f'SELECT curr_price FROM Stocks WHERE ticker="{ticker}"')
+    price = cur.fetchone()[0]
+    
     new_quantity = curr_quantity - quantity
     update_current_holding(account, ticker, new_quantity, avg)
+
+    curr_bal = get_account_cash(account)
+    update_account_cash(account, curr_bal + (quantity * price))
     return True
 
 
@@ -43,4 +58,17 @@ def update_current_holding(account, ticker, quantity, avg):
     db.commit()
 
 
-sell_stock(1, "META", 100)
+def get_account_cash(account):
+    cur.execute(f'SELECT cash FROM Accounts WHERE account_id="{account}"')
+    bal = cur.fetchone()
+
+    if not bal:
+        print("This account does not exist")
+        return None
+    return bal[0]
+
+def update_account_cash(account, cash):
+    cur.execute(f'UPDATE Accounts SET cash = {cash} WHERE account_id="{account}"')
+    db.commit()
+
+sell_stock(1, "AAPL", 50)
