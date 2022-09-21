10a5fd29ea999de546f4988c5c22dcbae4e2164d
NerosJourney <sandman4264@gmail.com>
2022-09-21 00:02:01 -0400

    added sell_stock

--- src/stock_manager.py
+++ src/stock_manager.py
@@ -3,11 +3,25 @@ from db_builder import db, cur
 def buy_stock(account, ticker, quantity):
     cur.execute(f'SELECT curr_price FROM Stocks WHERE ticker="{ticker}"')
     price = cur.fetchone()[0]
+
     curr_quantity, curr_avg_price = get_current_holding(account, ticker)
     new_quantity = curr_quantity + quantity
+    
     total_holdings_price = (curr_quantity * curr_avg_price) + (quantity * price)
     new_avg = total_holdings_price / new_quantity
+
     update_current_holding(account, ticker, new_quantity, new_avg)
+    return True
+
+
+def sell_stock(account, ticker, quantity):
+    curr_quantity, avg = get_current_holding(account, ticker)
+    if(quantity > curr_quantity):
+        print("ERROR! YOU DON'T OWN THAT STOCK")
+        return False
+    new_quantity = curr_quantity - quantity
+    update_current_holding(account, ticker, new_quantity, avg)
+    return True
 
 
 def get_current_holding(account, ticker):
@@ -20,9 +34,13 @@ def get_current_holding(account, ticker):
         cur.execute(f'INSERT INTO Holdings (account_id, ticker, quantity, avg_price) VALUES ({account}, "{ticker}", 0, 0)')
         curr_quantity = 0
         curr_avg_price = 0
+        db.commit()
     return curr_quantity, curr_avg_price
 
 
 def update_current_holding(account, ticker, quantity, avg):
     cur.execute(f'UPDATE Holdings SET quantity = {quantity}, avg_price = {avg} WHERE ticker="{ticker}" AND account_id="{account}"')
     db.commit()
+
+
+sell_stock(1, "META", 100)
